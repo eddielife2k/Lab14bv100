3d0f8296a79d09dcbf8c43842170ac7039549d5e
Eddie Hines <44607352+eddielife2k@users.noreply.github.com>
2019-03-07 09:52:42 -0500

    Update Lab14bv80.java

--- Lab 14b/Lab14bv80.java
+++ Lab 14b/Lab14bv80.java
@@ -11,15 +11,15 @@ public class Lab14bv80 extends Applet
 {
 	public void paint(Graphics g)
 	{
-      Train train = new Train(100,300);
-      train.addCar("Locomotive",Color.blue);
-      train.addCar("PassengerCar",Color.gray);
-      train.addCar("PassengerCar",Color.gray);
-      train.addCar("FreightCar",Color.green);
-      train.addCar("PassengerCar",Color.gray);
-      train.addCar("FreightCar",Color.green);
-      train.addCar("Caboose",Color.red);
-      train.addCar("FreightCar",Color.green);
+      Train train = new Train(g, 100,300);
+      train.addCar(g, "Locomotive",Color.blue);
+      train.addCar(g, "PassengerCar",Color.gray);
+      train.addCar(g, "PassengerCar",Color.gray);
+      train.addCar(g, "FreightCar",Color.green);
+      train.addCar(g, "PassengerCar",Color.gray);
+      train.addCar(g, "FreightCar",Color.green);
+      train.addCar(g, "Caboose",Color.red);
+      train.addCar(g, "FreightCar",Color.green);
       train.showCars(g);
 	}
 }
