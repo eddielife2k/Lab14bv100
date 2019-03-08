a5ff5883aaada61b653e4e09da39476554d943a2
Eddie Hines <44607352+eddielife2k@users.noreply.github.com>
2019-03-08 12:15:35 -0500

    Update Train.java

--- Lab 14b/Train.java
+++ Lab 14b/Train.java
@@ -6,29 +6,30 @@ public class Train
     private ArrayList<RailCar> t;
     private int x;
     private int y;
-    public Train(Graphics g, int x, int y){
+    public Train(int x, int y){
         this.x = x;
         this.y = y;
+        t = new ArrayList<RailCar>();
     }
-    public void addCar(Graphics g, String name, Color c){
-        int xC = t.size
+    public void addCar(String name, Color c){
+        int xC = t.size();
         if(name.equals("Locomotive")){
             RailCar loc = new Locomotive(c, xC, y);
             t.add(loc);
             
         }
         else if(name.equals("Passengercar")){
-            RailCar pas = new PassengerCar(c);
+            RailCar pas = new PassengerCar(c, xC, y);
             t.add(pas);
             
         }
         else if(name.equals("FreightCar")){
-            RailCar frt = new FreightCar(c);
+            RailCar frt = new FreightCar(c, xC, y);
             t.add(frt);
             
         }
         else if(name.equals("Caboose")){
-            RailCar cab = new Caboose(c);
+            RailCar cab = new Caboose(c, xC, y);
             t.add(cab);
             
         }
